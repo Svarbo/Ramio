a0e5a23d5923d79c0aeb032fbb65c170a2b20885
Svarbo <svarbo@mail.ru>
2023-11-16 07:36:45 +1100

    Доделал все уровни и jumpBoost

--- Assets/Scripts/Buffs/JumpBoost.cs
+++ Assets/Scripts/Buffs/JumpBoost.cs
@@ -3,25 +3,54 @@ using UnityEngine;
 [RequireComponent(typeof(Animator))]
 public class JumpBoost : MonoBehaviour
 {
+    [SerializeField] private float _reloadingTime;
+
     private Animator _animator;
-    private int _isCollectedAnimation = Animator.StringToHash("Collected");
+    private int _collectedAnimation = Animator.StringToHash("Collected");
+    private int _idleAnimation = Animator.StringToHash("Idle");
+    private float _currentReloadingTime = 0;
+    private bool _isCollected = false;
 
     private void Awake()
     {
         _animator = GetComponent<Animator>();
     }
 
+    private void Update()
+    {
+        TryReload();
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.TryGetComponent<ExtraJumpChecker>(out ExtraJumpChecker extraJumpChecker))
+        if (_isCollected == false)
         {
-            extraJumpChecker.AddExtraJump();
-            _animator.Play(_isCollectedAnimation);
+            if (collision.TryGetComponent<ExtraJumpChecker>(out ExtraJumpChecker extraJumpChecker))
+            {
+                extraJumpChecker.AddExtraJump();
+                _animator.Play(_collectedAnimation);
+            }
+        }
+    }
+
+    private void TryReload()
+    {
+        if (_isCollected == true)
+        {
+            _currentReloadingTime += Time.deltaTime;
+
+            if (_currentReloadingTime >= _reloadingTime)
+            {
+                _isCollected = false;
+                _currentReloadingTime = 0;
+
+                _animator.Play(_idleAnimation);
+            }
         }
     }
 
     public void Disable()
     {
-        gameObject.SetActive(false);
+        _isCollected = true;
     }
 }
