b8f25e1a516cd18bb1ca94049c9ba01fe7073206
Svarbo <svarbo@mail.ru>
2023-12-07 20:30:48 +1100

    New spawnpoint logic

--- Assets/Scripts/Level/SpawnPoints/SpawnPointTrigger.cs
+++ Assets/Scripts/Level/SpawnPoints/SpawnPointTrigger.cs
@@ -6,17 +6,26 @@ using UnityEngine.SceneManagement;
 
 namespace Level.SpawnPoints
 {
+    [RequireComponent(typeof(Collider))]
     [RequireComponent(typeof(AudioSource))]
+    [RequireComponent(typeof(SpriteRenderer))]
     public class SpawnPointTrigger : MonoBehaviour
     {
         [SerializeField] private AudioClip _audioClip;
+        [SerializeField] private Sprite _activatedSprite;
 
         [field: SerializeField] public int Index { get; private set; }
 
+        private Collider _collider;
         private AudioSource _audioSource;
+        private SpriteRenderer _spriteRenderer;
 
-        private void Awake() =>
+        private void Awake()
+        {
+            _collider = GetComponent<Collider>();
             _audioSource = GetComponent<AudioSource>();
+            _spriteRenderer = GetComponent<SpriteRenderer>();
+        }
 
         private void OnTriggerEnter2D(Collider2D collider)
         {
@@ -24,7 +33,9 @@ namespace Level.SpawnPoints
             {
                 Easy easy = LevelsProgress.Instance.GetDifficultByType(typeof(Easy)) as Easy;
                 easy.ChangeSpawnPoint(SceneManager.GetActiveScene().name, new SceneSpawnPoint(Index, transform.position));
-                gameObject.SetActive(false);
+
+                _spriteRenderer.sprite = _activatedSprite;
+                _collider.enabled = false;
 
                 _audioSource.PlayOneShot(_audioClip);
             }
